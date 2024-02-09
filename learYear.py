2a25283b2bc9203b47771e25e16ec41c708259f2
Utkarsh Pratap Singh <149144723+utkarsh-gith@users.noreply.github.com>
2024-02-09 17:52:32 +0530

    Create learYear.py

---
+++ learYear.py
@@ -0,0 +1,8 @@
+year = int(input("Enter the year:"))
+if (year % 4 == 0 and year % 100 !=0) or year % 400 == 0 :
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
+
+# Enter the year:2100
+# 2100 is not a leap year
